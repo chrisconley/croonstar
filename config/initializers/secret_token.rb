b70f3bf850ca5678918ff83daaa5b58418718f6d
jconley <conley.jj@gmail.com>
2010-10-15 20:52:06 -0400

    Bare Rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CroonStar::Application.config.secret_token = 'fbb3474e2335ca237ea610115fe94900bc06cafa746ee07dfa63e242ad4626c5026c259d92b36f4d13cb2cf04eca836d07a0ee38940387eba6df1eb36207dd27'
