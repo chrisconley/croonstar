00c545bfda1e8e5742c2fb8672f869f2482da449
jconley <conley.jj@gmail.com>
2010-10-16 14:41:31 -0400

    Added dynamic number calling

--- app/controllers/tropo_sessions_controller.rb
+++ app/controllers/tropo_sessions_controller.rb
@@ -1,7 +1,12 @@
 class TropoSessionsController < ApplicationController
   def create
+    sessions_object = Tropo::Generator.parse request.env['rack.input'].read
+    number_to_dial = sessions_object[:session][:parameters][:number_to_dial]
+    require 'pp'
+    pp sessions_object.inspect
+
     tropo = Tropo::Generator.new do
-              call :to => "+14846787619"
+              call :to => 'tel:+1' + number_to_dial
               on :event => 'hangup', :next => '/tropo_sessions/hangup.json'
               on :event => 'continue', :next => '/tropo_sessions/start_recording.json'
               ask({ :name    => 'ready',
@@ -10,7 +15,7 @@ class TropoSessionsController < ApplicationController
                     :timeout => 30,
                     :require => 'true' }) do
                       say     :value => 'Hello! Press 1 when your are ready to start crooning.'
-                      choices :value => 1
+                      choices :terminator => '1'
                     end
               end
     render :json => tropo.response
--- spec/acceptance/tropo_sessions_spec.rb
+++ spec/acceptance/tropo_sessions_spec.rb
@@ -7,11 +7,18 @@ feature "Tropo Sessions", %q{
 } do
 
   scenario "TropoSessions#create.json" do
+    number_to_dial = '12345'
+    Tropo::Generator.stub!(:parse).and_return({:session=>{:parameters=>{:number_to_dial=>number_to_dial}}})
     page.driver.post '/tropo_sessions/create.json'
 
+    page.should have_content(number_to_dial)
     page.should have_content('Hello! Press 1 when your are ready to start crooning.')
     page.should have_content('ready')
     page.should have_content('/tropo_sessions/start_recording.json')
     page.should have_content('/tropo_sessions/hangup.json')
+
   end
 end
+
+#Example Session object sent by Tropo
+#{"session"=>{"timestamp"=>"2010-10-16T18:19:56.542Z", "parameters"=>{"token"=>"b271522390e9344386bab43e7c786b81ccff6ce46213d49c6efb629f74b1af5b80db554cecaeaf0b4d218e2a", "action"=>"create", "number_to_dial"=>"4846787619"}, "callId"=>nil, "userType"=>"NONE", "id"=>"e7c1f8c3328b5785df4cea8d90a2f3ab", "initialText"=>nil, "accountId"=>"50694"}}
