1b253d492eb698ec56c8f55764f861462b6ced49
jconley <conley.jj@gmail.com>
2010-10-16 23:22:15 -0400

    Added get request to tropo

--- app/models/croon.rb
+++ app/models/croon.rb
@@ -6,8 +6,11 @@ class Croon
    end
 
    validates_presence_of :phone_number
-   validates_length_of :phone_number, :is => 10, :message => "must contain of 10 digits"
+   validates_length_of :phone_number, :is => 10, :message => "must contain 10 digits"
 
   field :phone_number
   field :song_url
+  field :vote_count, :type => Integer, :default => 0
+  field :crooner
+  referenced_in :song
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,19 @@
 class Song
   include Mongoid::Document
 
-  key :lyrics
-  key :url
-  key :title
+  field :lyrics
+  field :url
+  field :title
+  field :artist
+  references_many :croons
+
+  def self.select_options
+    options = []
+    puts "hello?"
+    all.each do |s|
+      puts "object" 
+      options << [ "#{s.title} by #{s.artist}", s.url ]
+    end
+    options
+  end
 end
