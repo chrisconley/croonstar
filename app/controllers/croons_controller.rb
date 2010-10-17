12e31333e21a07f16a7140851b80cc42a6fb809a
jconley <conley.jj@gmail.com>
2010-10-17 16:14:37 -0400

    Finished pagination

--- app/controllers/croons_controller.rb
+++ app/controllers/croons_controller.rb
@@ -1,16 +1,15 @@
 class CroonsController < ApplicationController
   def index
     @songs = Song.all
-    page = params[:page]  || 1
-    @croons = Croon.paginate(:page => page, :per_page => 15, :conditions => :recording_filename.exists => true )
-    p @croons
+    page = params[:page] || 1
+    @croons = Croon.where(:recording_filename.exists => true).order_by(:time_stamps.desc).paginate(:page => page, :per_page => 15)
     @croon = Croon.new
   end
 
   def create
     @croon = Croon.new(params[:croon])
     if @croon.save
-      AwesomeHTTP.get("http://api.tropo.com/1.0/sessions?action=create&token=b271522390e9344386bab43e7c786b81ccff6ce46213d49c6efb629f74b1af5b80db554cecaeaf0b4d218e2a", :croon_id => @croon.id)
+      AwesomeHTTP.get("http://api.tropo.com/1.0/sessions?action=create&token=#{TROPO_APP_TOKEN}", :croon_id => @croon.id)
       redirect_to croon_url(@croon)
     else
       @songs = Song.all
@@ -19,15 +18,7 @@ class CroonsController < ApplicationController
     end
   end
 
-  def crooning
-  end
-
   def show
     @croon = Croon.criteria.id(params[:id]).first
   end
-
-  def listen
-
-  end
-
 end
