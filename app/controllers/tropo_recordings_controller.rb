287a06039971668c551964d77122a2959ecf7cf5
jconley <conley.jj@gmail.com>
2010-10-17 15:20:57 -0400

    TropoRecording save instead of save

--- app/controllers/tropo_recordings_controller.rb
+++ app/controllers/tropo_recordings_controller.rb
@@ -5,7 +5,7 @@ class TropoRecordingsController < ApplicationController
     @croon = Croon.criteria.id(params[:croon_id]).first
     @croon.recording = params[:filename]
     @croon.status = "complete"
-    if @croon.save!
+    if @croon.save
       render :json => {:success => true}.to_json
     else
       render :json => {:success => false}.to_json
