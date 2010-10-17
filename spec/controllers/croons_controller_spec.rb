79d81f261b59c663b64727da8bb77d9069da56a0
Chris Conley <chris.m.conley@gmail.com>
2010-10-17 12:31:15 -0400

    added mp3 player

--- spec/controllers/croons_controller_spec.rb
+++ spec/controllers/croons_controller_spec.rb
@@ -3,33 +3,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe CroonsController do
 
   describe "GET 'index'" do
-    it "should be successful" do
-      Croon = mock
-      Croon.stub!(:all).return([])
-      get 'index'
-      response.should be_success
-    end
-  end
 
-  describe "GET 'create'" do
-    it "should be successful" do
-#      get 'create'
-#      response.should be_success
-    end
-  end
-
-  describe "GET 'crooning'" do
-    it "should be successful" do
-#      get 'crooning'
-#      response.should be_success
-    end
-  end
-
-  describe "GET 'show'" do
-    it "should be successful" do
-#      get 'show'
-#      response.should be_success
-    end
   end
 
 end
